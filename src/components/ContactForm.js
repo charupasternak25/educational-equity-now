6d957f3bf957e9adecbc16b38af72f1c25985e1c
Charu Pasternak <charupasternak25@gmail.com>
2021-02-25 23:28:19 -0800

    updated form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -15,7 +15,20 @@ const StyledConatiner = styled.div`
     }
 `;
 
-class Form extends Component {
+const StyledThankYouHeader = styled.div`    
+    font-size: 4rem;    
+    padding-top: 0;
+    padding-bottom: 1rem;
+    font-weight: 400;
+`;
+
+const StyledThankYouDescription = styled.div`
+    font-size: 1.8rem;
+    letter-spacing: 0.03px;
+    padding-bottom: 2rem;
+`;
+
+class ContactForm extends Component {
     constructor(props) {
         super(props);
         this.state = { 
@@ -23,7 +36,8 @@ class Form extends Component {
             lastname: "",
             email: "",
             phone: "",
-            message: "" 
+            message: "",
+            setViewToThankYou: false 
         };
     }
 
@@ -33,20 +47,41 @@ class Form extends Component {
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: encode({ "form-name": "contact-us", ...this.state })
         })
-        .then(() => window.location.href = '/thankyou')
+        .then(() => {
+            console.log('Submitted');
+            console.log('state:', this.state);
+            this.setState({ setViewToThankYou: true})
+        }
+        )
         .catch(error => alert(error));
         e.preventDefault();
     };
 
-    handleChange = e => this.setState({ [e.target.name]: e.target.value });
-
-    render() {
+    handleChange = (e) => {
         
+        this.setState({ [e.target.name]: e.target.value });
+        
+
+    }
+
+
+
+    render() {        
         const { firstname, lastname, email, phone, message } = this.state;
+        
         return (
             <StyledConatiner className="container">
+                {this.state.setViewToThankYou ? 
+                
+                <div className="row card padding center">
+                    <StyledThankYouHeader>Thank you for contacting us!</StyledThankYouHeader>                    
+                    <StyledThankYouDescription>We will get back to you within 24 business hours. <br />Until then, <a href="/">Keep learning!!</a></StyledThankYouDescription>
+                </div>
+                
+                : 
                 <div className="row card padding-5">
-                    <form data-netlify="true" data-netlify-recaptcha="true" className="col s12" onSubmit={this.handleSubmit}>
+                    <form data-netlify="true" data-netlify-honeypot="bot-field" className="col s12" onSubmit={this.handleSubmit}>
+                    <input type="hidden" name="form-name" value="contact-us" />
                         <div className="row">
                             <div className="input-field col s6">
                                 <input name="firstname" value={firstname} id="firstname" type="text" className="validate" onChange={this.handleChange} />
@@ -84,10 +119,11 @@ class Form extends Component {
                             </div>                            
                         </div>
                     </form>
-                </div>               
+                </div>   
+    }            
             </StyledConatiner>
         );
     }
 }
 
-export default Form;
+export default ContactForm;
