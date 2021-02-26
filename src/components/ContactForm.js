6ae916ef724146a1c33cabb06edbeba9b64cd954
Charu Pasternak <charupasternak25@gmail.com>
2021-02-25 23:43:17 -0800

    form submission try

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -57,11 +57,10 @@ class ContactForm extends Component {
         e.preventDefault();
     };
 
-    handleChange = (e) => {
-        
+    handleChange = (e) => {        
         this.setState({ [e.target.name]: e.target.value });
-        
-
+        console.log('e.target.name', e.target.name);
+        console.log('e.target.value', e.target.value);
     }
 
 
@@ -80,33 +79,33 @@ class ContactForm extends Component {
                 
                 : 
                 <div className="row card padding-5">
-                    <form data-netlify="true" data-netlify-honeypot="bot-field" className="col s12" onSubmit={this.handleSubmit}>
+                    <form data-netlify="true" className="col s12" onSubmit={this.handleSubmit}>
                     <input type="hidden" name="form-name" value="contact-us" />
                         <div className="row">
                             <div className="input-field col s6">
-                                <input name="firstname" value={firstname} id="firstname" type="text" className="validate" onChange={this.handleChange} />
+                                <input name="firstname" value={firstname} type="text" className="validate" onChange={this.handleChange} />
                                 <label for="firstname">First name</label>
                             </div>
                             <div className="input-field col s6">
-                                <input name="lastname" value={lastname} id="lastname" type="text" className="validate" onChange={this.handleChange} />
+                                <input name="lastname" value={lastname} type="text" className="validate" onChange={this.handleChange} />
                                 <label for="lastname">Last name</label>
                             </div>
                         </div>
 
                         <div className="row">
                             <div className="input-field col s6">
-                                <input name="email" value={email} id="email" type="email" className="validate" onChange={this.handleChange} />
+                                <input name="email" value={email} type="email" className="validate" onChange={this.handleChange} />
                                 <label for="email" data-error="wrong" data-success="right">Email</label>
                             </div>
                             <div className="input-field col s6">
-                                <input name="phone" value={phone} id="phone" type="tel" pattern="^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$" className="validate" onChange={this.handleChange} />
+                                <input name="phone" value={phone} type="tel" pattern="^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$" className="validate" onChange={this.handleChange} />
                                 <label for="phone">Phone</label>
                             </div>
                         </div>
 
                         <div className="row">
                             <div className="input-field col s12">
-                                <textarea name="message" value={message} id="message" className="materialize-textarea" onChange={this.handleChange} />
+                                <textarea name="message" value={message} className="materialize-textarea" onChange={this.handleChange} />
                                 <label for="message">Message</label>
                             </div>                            
                         </div>
