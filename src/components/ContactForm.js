330bd8248bc5ab832db18db21aa9fe12f85f1d02
Charu Pasternak <charupasternak25@gmail.com>
2021-02-26 14:49:38 -0800

    removed the state

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -61,12 +61,12 @@ class ContactForm extends Component {
         const { firstname, lastname, email, phone, message } = this.state;        
         return (
             <StyledConatiner className="container">
-                {this.state.setViewToThankYou ?                 
+                {/* {this.state.setViewToThankYou ?                 
                     <div className="row card padding center">
                         <StyledThankYouHeader>Thank you for contacting us!</StyledThankYouHeader>                    
                         <StyledThankYouDescription>We will get back to you within 24 business hours. <br />Until then, <a href="/">Keep learning!!</a></StyledThankYouDescription>
                     </div>
-                : 
+                :  */}
                 <div className="row card padding-5">
                     <form data-netlify="true" data-netlify-honeypot="bot-field" className="col s12" onSubmit={this.handleSubmit}>
                     <input type="hidden" name="form-name" value="contact-us" />
@@ -108,7 +108,7 @@ class ContactForm extends Component {
                         </div>
                     </form>
                 </div>   
-                }            
+                {/* }             */}
             </StyledConatiner>
         );
     }
